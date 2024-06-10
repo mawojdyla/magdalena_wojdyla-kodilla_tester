09d5e37cef3258ab4b07a38807b6d937eae07162
Magdalena Wojdyła <wojdylaamagda@gmail.com>
2024-06-10 14:57:32 +0200

    Deklarowanie i wywoływanie zmiennych

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -4,6 +4,14 @@ public class HelloWorld {
         System.out.println("Magdalena");
         System.out.println("35");
         System.out.println("W");
+//        String welcome = "Hello World";
+//        System.out.println(welcome);
+//        String myName = "Magdalena";
+//        System.out.println(myName);
+//        int myAge = 30;
+//        System.out.println(myAge);
+//        char InitialOfLastName = 'W';
+//        System.out.println(InitialOfLastName);
     }
 }
 
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String programmingSchool = "Kodilla";
+        String nameOfCourse = "Tester automatyzujący";
+        int durationCourseWeek = 16;
+        double priceCourse = 8999.00;
+        char initialOfNameOfLang = 'J';
+        boolean isOnline = true;
+        
+        System.out.println(programmingSchool);
+        System.out.println(nameOfCourse);
+        System.out.println(durationCourseWeek + " weeks");
+        System.out.println(priceCourse + " zł");
+        System.out.println(initialOfNameOfLang);
+        System.out.println(isOnline);
+    }
+}
