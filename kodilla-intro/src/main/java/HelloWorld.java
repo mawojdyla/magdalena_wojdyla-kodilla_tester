7abf7f15c8d5da774c85d69a841125290a58c3c6
Magdalena Wojdyła <wojdylaamagda@gmail.com>
2024-06-10 10:24:48 +0200

    Zaktualizowanie pliku HelloWorld.java z pełnym kodem

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Magdalena");
+        System.out.println("35");
+        System.out.println("W");
+    }
 }
+
+
