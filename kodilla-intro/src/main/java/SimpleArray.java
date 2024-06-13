6cc6aad8791e2b33ce9c85e63d7ca551e8a9f42f
Magdalena Wojdyła <wojdylaamagda@gmail.com>
2024-06-13 18:31:08 +0200

    Tworzenie tablicy

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,10 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+      String[] animals = new String[] {"lion", "turtle", "donkey", "monkey", "zebra"} ;
+      String animal = animals[3];
+        System.out.println(animals[3]);
+        int numberOfElements = animals.length;
+        System.out.println("Moja tablica zawiera "+ numberOfElements + " elementów.");
+    }
+}
+
