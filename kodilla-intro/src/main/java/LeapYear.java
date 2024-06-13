d9c7a7deb412fbb5ec7554061f8d089c6d2f9cfd
Magdalena Wojdyła <wojdylaamagda@gmail.com>
2024-06-13 17:01:40 +0200

    Zaktualizowanie kodu z rokiem przestępnym

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class LeapYear {
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int year = 2021;
+        if (isLeapYear(year)) {
+            System.out.println(year + " jest rokiem przestępnym.");
+        } else {
+            System.out.println(year + " nie jest rokiem przestępnym.");
+        }
+    }
 }
