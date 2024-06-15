068e03a65453cdc3ef4d10dd620a29018d2a03f8
Magdalena Wojdyła <wojdylaamagda@gmail.com>
2024-06-15 16:40:36 +0200

    Aktualizacja pętli

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Loops {
+public class Loops {
+    static String[] names = {"Zygfryd", "Gwidon", "Florentyna"};
+    static int[] numbers = {16, 26, 88, 9, 14, 25};
+
+
+    public static void main(String[] args) {
+        // Iterowanie po tablicy names
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    // Tworzenie instancji klasy tylko do wywołania metody sumOfNumbers
+    int sum = new Loops().sumOfNumbers(numbers);
+    System.out.println("Total of numbers is " + sum) ;
+
+
+    }
+
+// Metoda sumOfNumbers pozostaje niestatyczna
+    public int sumOfNumbers(int[] numbers) {
+        int result = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            result += numbers[i];
+
+        }
+        return result;
+    }
 }
+
+
+
