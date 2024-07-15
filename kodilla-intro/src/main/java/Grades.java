36ad2b37a3d6a78c1a0f8a0f6b632121d217cd0f
Magdalena Wojdyła <wojdylaamagda@gmail.com>
2024-07-15 18:57:17 +0200

    Dodanie zadania Grades, Obiektowość

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -6,7 +6,7 @@ public class Grades {
     public Grades() {
         this.grades = new int[10];
         this.size = 0;
-        this.sum = 0;
+        this.sum = 0.0;
     }
 
     public void add(int value) {
@@ -25,18 +25,31 @@ public class Grades {
     }
 
     public double average() {
-//        if (this.size != 0) {
-//            return;
-//        }
-        sum = 0.0;
-        System.out.println("debug start");
-        System.out.println(this.size);
-        System.out.println("debug end");
-        for (int i = 0; i <= this.size; i++) {
-            sum = sum + grades[this.size];
-
+        if (this.size == 0) {
+            return 0;
         }
-        double average = sum / this.size;
-        return average;
+        sum = 0.0;
+        for (int i = 0; i < this.size; i++) {
+            sum = sum + grades[i];
+        }
+        return sum / this.size;
+    }
+
+    public static void main(String[] args) {
+        Grades myGrades = new Grades();
+        myGrades.add(8);
+        myGrades.add(6);
+        myGrades.add(3);
+        System.out.println("Last added grade: " + myGrades.lastAdded());
+        System.out.println("Average: " + myGrades.average());
     }
 }
+
+
+//  this.grades[this.size] = value;
+//  this.size++;
+//Użycie this.size jako indeksu oznacza,
+// że nowa ocena value zostanie dodana
+// na pierwsze wolne miejsce w tablicy grades.
+// Jeśli size wynosi 0, nowa ocena zostanie dodana na miejsce 0.
+// Jeśli size wynosi 1, nowa ocena zostanie dodana na miejsce 1 i tak dalej.
